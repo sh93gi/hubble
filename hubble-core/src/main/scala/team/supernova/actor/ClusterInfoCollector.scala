d049c616092fab3f400e663749f2c5b69035f68a
Saman SattariKhavas <saman.sattari.khavas@ing.nl>
2016-01-13 11:03:54 +0100

    stop all actors and app after one run

--- hubble-core/src/main/scala/team/supernova/actor/ClusterInfoCollector.scala
+++ hubble-core/src/main/scala/team/supernova/actor/ClusterInfoCollector.scala
@@ -1,18 +1,23 @@
 package team.supernova.actor
 
-import akka.actor.{Props, Actor, ActorLogging}
-import com.datastax.driver.core.Session
+import akka.actor._
 import team.supernova.confluence.soap.rpc.soap.actions.Token
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 object ClusterInfoCollector {
+
   case class Start(clusters: List[CassandraClusterGroup])
-  def props(space: String, token: Token): Props = Props(new ClusterInfoCollector(space, token))
+
+  def props(space: String, token: Token, system: ActorSystem): Props = Props(new ClusterInfoCollector(space, token, system))
 }
 
-class ClusterInfoCollector(sSpace: String, token: Token) extends Actor with ActorLogging {
+class ClusterInfoCollector(sSpace: String, token: Token, systemValue: ActorSystem) extends Actor with ActorLogging {
+  import context._
 
-  val group = context.system.actorOf(ClusterInfoController.props(self))
-  val pages = context.system.actorOf(ConfluencePage.props(sSpace, token))
+  val group = actorOf(ClusterInfoController.props(self))
+  val pages = actorOf(ConfluencePage.props(sSpace, token))
 
   override def receive: Receive = {
     case ClusterInfoCollector.Start(clusters) => {
@@ -25,6 +30,14 @@ class ClusterInfoCollector(sSpace: String, token: Token) extends Actor with Acto
     }
     case ConfluencePage.Done => {
       log.info("END: Confluence pages generated")
+      val result = Future {
+        system.shutdown()
+        system.awaitTermination()
+      }
+      result onComplete {
+        case Success(r) => if (systemValue.isTerminated) sys.exit()
+        case Failure(t) => println("An error has occured: " + t.getMessage)
+      }
     }
   }
 }
