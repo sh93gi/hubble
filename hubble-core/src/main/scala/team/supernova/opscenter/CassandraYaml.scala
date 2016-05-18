132c8663087df95e0a9db2b88fc553e7887ecd73
Gerben Oostra <gerben.oostra@ing.nl>
2016-05-18 10:55:54 +0200

    More explicit intent of CassandraYaml to reference inner .all map for all properties

--- hubble-core/src/main/scala/team/supernova/opscenter/CassandraYaml.scala
+++ hubble-core/src/main/scala/team/supernova/opscenter/CassandraYaml.scala
@@ -9,7 +9,6 @@ import org.json4s.{DefaultFormats, _}
 object CassandraYaml {
  def parseBody (body: String) : CassandraYaml = {
   implicit val formats = DefaultFormats
-  //parse(body).extract[CassandraYaml]
   CassandraYaml(parse(body).extract[Map[String, Any]])
  }
 }
@@ -19,12 +18,12 @@ object CassandraYaml {
   *
   * @param all : the yaml file, parsed as Map to be able to list all elements
   */
-case class CassandraYaml(all: Map[String, Any]){
+case class CassandraYaml(val all: Map[String, Any]){
 
- def authenticator: Option[String] = all.get("authenticator").map(_.asInstanceOf[String])
- def authority : Option[String] = all.get("authority").map(_.asInstanceOf[String])
- def auto_snapshot : Option[Boolean] = all.get("auto_snapshot").map(_.asInstanceOf[Boolean])
- def cluster_name : Option[String] = all.get("cluster_name").map(_.asInstanceOf[String])
- def concurrent_reads : Option[Int] = all.get("concurrent_reads").map(_.asInstanceOf[Int])
- def concurrent_writes : Option[Int] = all.get("concurrent_writes").map(_.asInstanceOf[Int])
+ lazy val authenticator: Option[String] = all.get("authenticator").map(_.asInstanceOf[String])
+ lazy val authority : Option[String] = all.get("authority").map(_.asInstanceOf[String])
+ lazy val auto_snapshot : Option[Boolean] = all.get("auto_snapshot").map(_.asInstanceOf[Boolean])
+ lazy val cluster_name : Option[String] = all.get("cluster_name").map(_.asInstanceOf[String])
+ lazy val concurrent_reads : Option[Int] = all.get("concurrent_reads").map(_.asInstanceOf[Int])
+ lazy val concurrent_writes : Option[Int] = all.get("concurrent_writes").map(_.asInstanceOf[Int])
 }
