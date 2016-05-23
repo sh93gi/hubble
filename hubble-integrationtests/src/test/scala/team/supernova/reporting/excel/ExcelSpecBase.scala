d139f585bded1c1530bd079e578061ce9f1c31b5
Gerben Oostra <gerben.oostra@ing.nl>
2016-05-23 11:27:36 +0200

    Moved hubble-reporting integration tests (base classes) to hubble-integrationtests

--- hubble-integrationtests/src/test/scala/team/supernova/reporting/excel/ExcelSpecBase.scala
+++ hubble-integrationtests/src/test/scala/team/supernova/reporting/excel/ExcelSpecBase.scala
@@ -1,19 +1,15 @@
 package team.supernova.reporting.excel
 
-import java.util.Properties
-
 import com.typesafe.config.ConfigFactory
 import org.scalatest._
+import team.supernova.HubbleApp
 import team.supernova.reporting.confluence.{ConfluenceToken, Overview}
-/**
- * Created by Gary Stewart on 4-8-2015.
- *
- */
 
-class ExcelSpec extends FunSpecLike {
+abstract class ExcelSpecBase extends FunSpecLike {
 
   val config = ConfigFactory.load()
-  val space = config.getString("hubble.confluence.space")
+  val space = HubbleApp.mapConfigToConfluenceSpace(config)
+
   it  ("Test Create excel sheet") {
     Excel.createSheet (Overview.getKeyspacesInfoFromManualPage (ConfluenceToken.getConfluenceToken (config), space ))
   }
