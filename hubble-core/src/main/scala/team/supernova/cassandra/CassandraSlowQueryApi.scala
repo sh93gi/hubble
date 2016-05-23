9c8df577e627865605fdbd4df4da799608736b30
Gerben Oostra <gerben.oostra@ing.nl>
2016-05-23 11:28:23 +0200

    hasSlowQueryData also verifies that it has access to dse_perf.node_slow_log table.

--- hubble-core/src/main/scala/team/supernova/cassandra/CassandraSlowQueryApi.scala
+++ hubble-core/src/main/scala/team/supernova/cassandra/CassandraSlowQueryApi.scala
@@ -2,7 +2,8 @@ package team.supernova.cassandra
 
 import java.net.InetAddress
 
-import com.datastax.driver.core.exceptions.ReadTimeoutException
+import com.datastax.driver.core.Session
+import com.datastax.driver.core.exceptions.{QueryTimeoutException, ReadTimeoutException, UnauthorizedException}
 import com.sun.mail.iap.ConnectionException
 import org.joda.time.DateTime
 import org.slf4j.LoggerFactory
@@ -18,12 +19,23 @@ class CassandraSlowQueryApi(cluster: ClusterEnv) {
       using(newSession()){
         session =>{
           try{
-          val dsePerformanceCount = retryExecute(
-            {session.execute("select count(*) from system.schema_columnfamilies where keyspace_name='dse_perf' and columnfamily_name='node_slow_log';").asScala},
-            3)
-            return dsePerformanceCount.head.getLong(0)>0
+            val dsePerformanceCount = retryExecute(
+              {session.execute("select count(*) from system.schema_columnfamilies where keyspace_name='dse_perf' and columnfamily_name='node_slow_log';").asScala},
+              3)
+            if (dsePerformanceCount.head.getLong(0)==0)
+              return false
+            queryNodeSlowLog(session, Some(1), _=>{})  // Verify if table can be queried without exceptions (permissions)
+            return true
           }catch{
-            case e: ConnectionException => return false
+            case e: QueryTimeoutException =>
+              log.error(s"Got timeout querying system.schema_columnfamilies: ${e.getMessage}")
+              return false
+            case e: ConnectionException =>
+              log.error(s"Got connectionexception while determining availability of node_slow_log: ${e.getMessage}")
+              return false
+            case e: UnauthorizedException =>
+              log.warn(s"The dse_perf.node_slow_log exists but the user used by hubble is unauthorized to read from it: ${e.getMessage}")
+              return false
           }
         }
       }
@@ -33,43 +45,47 @@ class CassandraSlowQueryApi(cluster: ClusterEnv) {
     if(!hasSlowQueryData()){
       return
     }
+    using(newSession()) { session => queryNodeSlowLog(session, limit, op)}
+  }
+
+  def queryNodeSlowLog(session: Session, limit: Option[Int], op: (SlowQuery) => Unit): Unit = {
     val limit_text = limit.map("limit %d".format(_)).getOrElse("")
     var count = 0
-    using(newSession()) { session =>
-      if (limit.isDefined && count>=limit.get)
-        return
-      val query = session.prepare(s"select commands, table_names, duration from dse_perf.node_slow_log " +
-        s"WHERE node_ip=? " +
-        s"AND date=? " +
-        s"$limit_text;")
-      val nodes = session.execute("select peer from system.peers;").all().asScala.map(_.getInet("peer"))
-      var failed : Option[Throwable] = None
-      for (offset <- 0 to 7){
-        val midnight = CassandraDateTime.toMidnight(new DateTime().minusDays(offset)).toDate
-        for (node_ip <- nodes){
-          log.info(s"Getting slow queries for node $node_ip and date $midnight")
-          val node_inet = InetAddress.getByName(node_ip.getHostAddress)
-          if (limit.isDefined && count>=limit.get)
-            return //We are done, no more nodes/days to try
-          val statement = query.bind(node_inet, midnight)
-          try{
-            // Could timeout on retrieving the first page
-            val results = retryExecute(
-              {session.execute(statement).asScala},
-              3)
-            // Foreach can time out on getting the next page
-            results.foreach(r=>{
-              count+=1
-              if (limit.isEmpty || count<=limit.get)
-                op(SlowQuery(r))
-            })
-          }catch {
-            case e:ReadTimeoutException => failed=Some(e)
-          }
+    if (limit.isDefined && count >= limit.get)
+      return
+    val query = session.prepare(s"select commands, table_names, duration from dse_perf.node_slow_log " +
+      s"WHERE node_ip=? " +
+      s"AND date=? " +
+      s"$limit_text;")
+    val nodes = session.execute("select peer from system.peers;").all().asScala.map(_.getInet("peer"))
+    var failed: Option[Throwable] = None
+    for (offset <- 0 to 7) {
+      val midnight = CassandraDateTime.toMidnight(new DateTime().minusDays(offset)).toDate
+      for (node_ip <- nodes) {
+        log.info(s"Getting slow queries for node $node_ip and date $midnight")
+        val node_inet = InetAddress.getByName(node_ip.getHostAddress)
+        if (limit.isDefined && count >= limit.get)
+          return //We are done, no more nodes/days to try
+        val statement = query.bind(node_inet, midnight)
+        try {
+          // Could timeout on retrieving the first page
+          val results = retryExecute(
+            {
+              session.execute(statement).asScala
+            },
+            3)
+          // Foreach can time out on getting the next page
+          results.foreach(r => {
+            count += 1
+            if (limit.isEmpty || count <= limit.get)
+              op(SlowQuery(r))
+          })
+        } catch {
+          case e: ReadTimeoutException => failed = Some(e)
         }
       }
-      // After all requested days for all available nodes, throw exception if not all data could be retrieved
-      failed.foreach(throw _) //Rethrow exception if at least one occurred
     }
+    // After all requested days for all available nodes, throw exception if not all data could be retrieved
+    failed.foreach(throw _) //Rethrow exception if at least one occurred
   }
 }
