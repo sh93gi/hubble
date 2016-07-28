12bbcc388aae3b997e396375e041b550eb854274
Gerben Oostra <gerben.oostra@ing.nl>
2016-07-28 14:37:03 +0200

    Corrected format string in MaturitySection

--- hubble-core/src/main/scala/team/supernova/confluence/MaturitySection.scala
+++ hubble-core/src/main/scala/team/supernova/confluence/MaturitySection.scala
@@ -6,7 +6,6 @@ import scala.xml.{NodeSeq, Text}
 
 object MaturitySection {
   def present(header: String, maturities:Map[Int, List[MaturityAspect]]): NodeSeq ={
-
     <p><h2>{Text(header)}</h2>++
       { maturities.toList.sortBy(_._1).map { case (level, aspects) => presentLevel(level, aspects) } }++
     </p>
@@ -16,7 +15,7 @@ object MaturitySection {
     val base = aspects.map(_.weight).sum
     val ratio = aspects.filter(_.check.hasPassed).map(_.weight).sum / base
     val failed = aspects.filterNot(_.check.hasPassed).map {
-      case aspect => "for %.2f%: %s".format(100D*aspect.weight/base, aspect.check.details)
+      case aspect => "for %.2f%%: %s".format(100D*aspect.weight/base, aspect.check.details)
       }.sorted.mkString("\n")
     <h3>Maturity level {level} : {"%.2f".format(100D*ratio)}%</h3>++
       { Confluence.confluenceCodeBlock("Failed aspects", failed ,"none") }++
